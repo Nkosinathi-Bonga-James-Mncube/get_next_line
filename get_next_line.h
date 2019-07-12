ef20e2720426e4750b7041a2828e0eb8a3e412b9
Nkosinathi-Bonga-James-Mncube <nmncube@student.wethinkcode.co.za>
2019-07-12 10:52:33 +0200

    Working version 1

--- get_next_line.h
+++ get_next_line.h
@@ -1,6 +1,6 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-#define BUFF_SIZE 26// issue with buff size 11//issue with buff 26//issue with 29//issue with 100
+#define BUFF_SIZE 4// issue with buff size 11//issue with buff 26//issue with 29//issue with 100
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
