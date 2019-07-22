e447c0a089563e6b8ad7dd6c2b2b467fef9886c2
Nkosinathi MNCUBE <nmncube@c4r10s8.wethinkcode.co.za>
2019-07-22 17:02:21 +0200

    Final version

--- get_next_line.c
+++ get_next_line.c
@@ -4,29 +4,30 @@
 
 static char *ft_leftover(char **line , char *s1)
 {
-	char *s2;
-	int k;
+	int		k;
+	
 	k = 0;
-
 	while (s1[k] != '\0' && s1[k] != '\n')
 		k++;
-	*line = ft_strncpy(*line,s1,k);
+	*line=ft_memalloc(k);
+	ft_strncpy(*line,s1,k);
+	//printf("%s<-" , *line);
 	k++;
-       if (!ft_strcmp(*line,s1) == 0)
-       {
-	s1 = (char*)&s1[k];
-	return (s1);	
-       }
-       else
-	       return (NULL);
+	if (ft_strcmp(*line,s1) != 0)
+	{
+		s1 = (char*)&s1[k];
+		return (s1);
+	}
+	return (NULL);
 }
-char *ft_j_words(char *temp ,char *buff)
+/*char *ft_j_words(char *temp ,char *buff)
 {	char *s1;
 
 	s1 = ft_strjoin(temp, buff);
 	free(temp);
+	temp = s1;
 	return (s1);
-}
+}*/
 int get_next_line(const int fd, char **line)
 {
 	static char *temp[1024];
@@ -34,21 +35,29 @@ int get_next_line(const int fd, char **line)
 	char buff[BUFF_SIZE + 1];
 	int x;
 
-	x =1;
+	x = 1;
 	if (fd < 0 || !line || read(fd,buff,0) < 0 )
 		return (-1);
 	if (!temp[fd])
 		temp[fd] = ft_strnew(BUFF_SIZE);
 	while ((x = read(fd, buff, BUFF_SIZE)) >  0)
 	{
+		if (x == 0)//new addition
+			return (0);
+		if (x < 0)//new addition
+			return (-1);
+		//ft_strclr(s1);<-current solution
 		buff[x]='\0';
-		s1 = ft_j_words(temp[fd] , buff);
+		s1 = ft_strjoin(temp[fd] ,buff);
+		//s1 = ft_j_words(temp[fd] , buff);
+		free(temp[fd]);
 		temp[fd] =s1;
 		if (ft_strchr(temp[fd], '\n'))
 			break ;
+		//ft_strclr(s1);
 	}
 	if (x < BUFF_SIZE && !ft_strlen(temp[fd]))
-		return (0);	
+		return (0);
 	temp[fd]= ft_leftover(line, temp[fd]);
 	return (1);
 }
--- main.c
+++ main.c
@@ -4,11 +4,18 @@ int	main(int argc, char**argv)
 {
 	char *line2;
 	int fd;
+	int x;
 
 	fd = open (argv[1], O_RDONLY);
+	//get_next_line(fd , &line2);
 	while (get_next_line(fd,&line2) > 0)
 	{
 		printf("%s\n", line2);
+		//printf("%d\n" ,x);
+		free(line2);
+	}
+	while (1)
+	{
 	}
 	return (0);
 }
